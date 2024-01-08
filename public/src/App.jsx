0a634f0a1bb8b62e7077ce24902da4b233f0660f
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2024-01-08 09:18:53 -0500

    add react-router package, and pseudo the route

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="bg-blue-500 p-8 text-white">
+
+      {/* <Routes>
+        <Route path="/" element={<Main pic={pic} />} />
+        <Route path="/aboutPage" element={<About />} />
+        <Route path="/kidVideosPage" element={<KidVideos />} />
+        <Route path="/resourcePage" element={<Resources />} />
+        <Route path="/tipsPage" element={<TipsTricks />} />
+        <Route path="/caregiverPage" element={<CaregiverVideos />} />
+      </Routes> */}
+
+
+
       <h1 className="text-4xl font-bold mb-4">Blue Background Component</h1>
       <p className="text-lg leading-relaxed mb-4">
         This component has a blue background using Tailwind CSS utility classes. You can customize the styles based on your design preferences.
--- public/src/index.js
+++ public/src/index.js
@@ -1,7 +1,11 @@
 import React, {Suspense} from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { BrowserRouter } from "react-router-dom";
 
 const node = document.getElementById('root');
 const root = createRoot(node);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>);
